06ee42c6900fb8fd17ec63c835124c67380e197d
mtaneva17 <mtaneva-student@azbuki-school.bg>
2022-03-07 11:47:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from curses import tigetflag
-
 
 class MyMath:
-    def __init__:
-        guza_ti
-        ebi_si_maikata kelesh
-        shte eba bashta ti
-        shte mu kaja sladkishche, podgotvi se da ne ti plashta  spotify-a <3
-        sugar daddy??
-        napomnq mi na nqkoi ;) poznai koi (ama ne go pishi, samo si go pomisli)
-        sus
-        ew
+    def __init__(self) -> None:
+        pass
+
+    
